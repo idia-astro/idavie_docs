bd7920dc5c21b02bb92de8bda30d186fcca3255f
Cilliers Pretorius <41905461+Pietersielie@users.noreply.github.com>
2026-08-04 17:18:46 +0200

    Update conf.py version number.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = u'IDIA Vislab, INAF-OACT'
 # The short X.Y version
 version = u'1.1'
 # The full version, including alpha/beta/rc tags
-release = u'1.1'
+release = u'1.1.0'
 
 # -- General configuration ---------------------------------------------------
 
